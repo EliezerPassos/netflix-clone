b17fc73b28afa9722eaf50451077224743132aeb
Eliezer <luccas_passos@hotmail.com>
2022-05-28 11:49:07 -0300

    comit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Tmdb from './Tmdb';
 
 function App(){
+  
+  useEffect(()=>{
+    
+    const loadAll = async () =>{
+      //pegando a list toda
+      let list = await Tmdb.getHomelist();
+      console.log(list);
+    }
+    loadAll();
+
+  }, []);
+  
+  
   return(
     <h1>Olá mundo</h1>
   );
--- src/Tmdb.js
+++ src/Tmdb.js
@@ -1,68 +1,56 @@
 const API_KEY  =  "ab668a3c0188cad32e62a77434f4f202";
 const API_BASE =  "https://api.themoviedb.org/3"
 
+const basicFecth = async (endpoint) => {
+    const req = await fetch(`${API_BASE}${endpoint}`);
+    const json = await req.json();
 
-/*
--Originais netflix
--recomendamos
--top rate
--ação
--comedia
--terror
--romance
--documentario
-*/
-
-export default{
-
-getHomelist: async ()=>{
-    return[
-
-        {
-            slug:  'originals',
-            title: 'originais do Netflix',
-            items: []
-        },
-
-        {
-            slug: 'trending',
-            title: 'Recomendados para você',
-            items: []
-        },
-
-        {
-            slug:  'toprated',
-            title: 'Em Alta',
-            items: []
-        },
-
-        {
-            slug:  'action',
-            title: 'Ação',
-            items: []
-        },
-        {
-            slug:  'comedy',
-            title: 'Comedia',
-            items: []
-        },
-        {
-            slug:  'horror',
-            title: 'terror',
-            items: []
-        },
-        {
-            slug:  'romance',
-            title: 'Romance',
-            items: []
-        },
-        {
-            slug:  'documentary',
-            title: 'Documentário',
-            items: []
-        },
-
-    ];
+    return json;
 }
 
+export default {
+    getHomeList: async () => {
+        return [
+            {
+                slug: 'originals',
+                title: 'Originais do Netflix',
+                items: await basicFecth(`/discover/tv?with_networks=213&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'trending',
+                title: 'Recomendados para Você',
+                items: await basicFecth(`/trending/all/week?&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'toprated',
+                title: 'Em Alta',
+                items: await basicFecth(`/movie/top_rated?&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'action',
+                title: 'Ação',
+                items: await basicFecth(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'commedy',
+                title: 'Comédia',
+                items: await basicFecth(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'horror',
+                title: 'Terror',
+                items: await basicFecth(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'romance',
+                title: 'Romance',
+                items: await basicFecth(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'documentary',
+                title: 'Documentários',
+                items: await basicFecth(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
+            }
+        ]
+    }
 }
