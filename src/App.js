31b0e8567aec246fb85f921c33554cd2ee64460a
Eliezer <luccas_passos@hotmail.com>
2022-05-28 13:49:50 -0300

    mais uma etapa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
+
 
 function App(){
   
+  const [movieList, setMovieList] = useState([]);
+
   useEffect(()=>{
-    
-    const loadAll = async () =>{
-      //pegando a list toda
-      let list = await Tmdb.getHomelist();
-      console.log(list);
+    const loadAll = async ()=>{
+      //pegando tota a lista
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
     }
+
     loadAll();
 
   }, []);
-  
-  
+
   return(
-    <h1>Olá mundo</h1>
+    <div className="page">
+
+      <section className='lista'>
+        {movieList.map((item,key)=>(
+         
+         <MovieRow key={key} title={item.title} items={item.items} />
+
+        ))}
+      </section>
+
+
+    </div>
   );
 }
+
 
 
 export default App;
---
+++ src/components/MovieRow.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./MovieRow.css";
+export default ({title, items})=> {
+    return(
+        <div>
+            <h2>{title}</h2>
+            <div className="movierow--listarea">
+                {items.results.length > 0 && items.results.map((item, key)=>(
+                    <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title}/>
+                ))}
+            </div>
+        </div>
+    );
+}
