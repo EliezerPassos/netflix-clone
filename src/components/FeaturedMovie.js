a09b52c57de3dba332fb6d8b46f28abe7ed4298b
Eliezer <luccas_passos@hotmail.com>
2022-05-30 17:57:55 -0300

    comit texto

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -11,6 +11,11 @@ export default ({item})=>{
         genres.push( item.genres[i].name);
 
     }
+
+    let description = item.overview;
+    if(description.lenght > 200){
+        description = description.substring(0, 200)+'...';
+    }
     
     return(
 
@@ -30,7 +35,7 @@ export default ({item})=>{
                      <div className='featured--seasons'>{item.number_of_seasons} Temporada{item.number_of_seasons !== 1 ? 's' :''}</div>
                  </div>
 
-                 <div className='featured-description'>{item.overview}</div>
+                 <div className='featured-description'>{description}</div>
                 <div className='featured--buttons'>
                     <a className='featured--watchbutton' href={`/watch/${item.id}`}>▶ Assistir</a>
                     <a className='featured--mylistbutton' href={`/list/add/${item.id}`}>+ Minha Lista</a>
